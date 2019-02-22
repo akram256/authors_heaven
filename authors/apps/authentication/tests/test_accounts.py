c70963b5968bd03c38e14896138cb71401d5aed8
a4a <mukasaakram55@gmail.com>
2019-02-22 09:48:54 +0300

    add more functionality

---
+++ authors/apps/authentication/tests/test_accounts.py
@@ -0,0 +1,91 @@
+from rest_framework import serializers, status
+from rest_framework.test import APITestCase
+from ..backends import JWTAuthentication
+from ..models import User
+
+
+class AccountTestCase(APITestCase):
+
+    def test_recieve_a_token_after_registration(self):
+        data = {
+            "user": {
+            "username": "akram",
+            "email": "mukasaakram@gmail.com",
+            "password": "Akram@2011"
+            }
+        }
+        response = self.client.post('/api/users/', data,format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        assert 'token' in response.data
+
+    def test_token_received_after_successful_login(self):
+        """
+        Test that a user will receive 
+        a token after successfull login
+        """
+        data = {
+            "user": {
+            "username": "akram",
+            "email": "mukasaakram@gmail.com",
+            "password": "Akram@2011"
+            }
+        }
+        response = self.client.post('/api/users/',data, format='json')
+        data = {"user": { "email": "mukasaakram@gmail.com", "password":"Akram@2011"}}
+        response = self.client.post('/api/users/login/',data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        assert 'token' in response.data
+
+    def test_user_can_get_token_for_current_user(self):
+        """
+        Test that a user will receive 
+        a token after successfull login
+        """
+        data = {
+            "user": {
+            "username": "akram",
+            "email": "mukasaakram@gmail.com",
+            "password": "Akram@2011"
+            }
+        }
+        response = self.client.post('/api/users/',data, format='json')
+        data = {
+            "user": {
+            "email": "mukasaakram@gmail.com",
+            "password": "Akram@2011"
+            }
+        }
+        response = self.client.post('/api/users/login/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        assert 'token' in response.data
+        # self.client.credentials(HTTP_AUTHORIZATION= 'Bearer ' + response.data['token'])
+        # response = self.client.get('/api/user/', format='json')
+
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)   
+
+    # def test_wrong_token_header_prefix(self):
+    #     """
+    #     Test when wrong authoriation header
+    #     prefix is entered
+    #     """
+    #     self.client.credentials(HTTP_AUTHORIZATION= 'hgfds ' + 'poiuytfd')
+    #     response = self.client.get("/api/user/", format="json")
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_for_invalid_token(self):
+        """
+        Test when an invalid 
+        authorisation header is provided
+        """
+        self.client.credentials(HTTP_AUTHORIZATION= 'Token ' + 'yyuug')
+        response = self.client.get("/api/user/", format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)  
+
+    def test_no_token_in_header(self):
+        """
+        Test when no authorization token
+        is entered in the header
+        """
+        self.client.credentials(HTTP_AUTHORIZATION= ' ' + 'shfdj')
+        response = self.client.get("/api/user/", format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN) 
--- authors/apps/authentication/tests/tests.py
+++ authors/apps/authentication/tests/tests.py
@@ -9,7 +9,7 @@ class Authentication(APITestCase):
     def test_for_new_user(self):
          # url = reverse('signup')
         data = {"user": {"username": "akram",
-                         "email": "akram@gmail.com", "password": "akrammukasa"}}
+                         "email": "akram@gmail.com", "password": "Akram@mukasa2"}}
         response = self.client.post('/api/users/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
    
@@ -17,33 +17,33 @@ class Authentication(APITestCase):
         """
         Method for testing if there is a missing username during registration.
         """
-        data = {"user": { "username":"", "email": "akram@gmail.com", "password":"akrammukasa"}}
+        data = {"user": { "username":"", "email": "akram@gmail.com", "password":"Akram@mukasa2"}}
         response = self.client.post('/api/users/' ,data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("This field may not be blank.", str(response.data))
+        self.assertIn("Please the username should be at least 4 characters long and above", str(response.data))
 
     def test_for_missing_email(self):
         """
         Method for testing if there is a missing email during registration.
         """
-        data = {"user": { "username":"mukasa", "email": "", "password":"akrammukasa"}}
+        data = {"user": { "username":"mukasa", "email": "", "password":"Akram@mukasa2"}}
         response = self.client.post('/api/users/' ,data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("This field may not be blank.", str(response.data))
+        self.assertIn("Please enter a valid email address", str(response.data))
 
     def test_login(self):
         # url = reverse('signup')
         data = {"user": {"username": "akram",
-                         "email": "akram@gmail.com", "password": "akrammukasa"}}
+                         "email": "akram@gmail.com", "password": "Akram@mukasa2"}}
         response = self.client.post('/api/users/', data, format='json')
         # url = reverse('login')
-        data = {"user": {"email": "akram@gmail.com", "password": "akrammukasa"}}
+        data = {"user": {"email": "akram@gmail.com", "password": "Akram@mukasa2"}}
         response = self.client.post('/api/users/login/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.get().email, "akram@gmail.com")
 
     def test_login_with_non_existing_user(self):
-        data = {"user": { "email": "akram@gmail.com", "password":"akrammukasa"}}
+        data = {"user": { "email": "akram@gmail.com", "password":"Akram@mukasa2"}}
         response = self.client.post('/api/users/login/',data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("A user with this email and password was not found.", str(response.data))
